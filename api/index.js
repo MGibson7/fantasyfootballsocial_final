ecd438aa0a78506f39ffc19d38801eccc1013634
MGibson7 <MGibsonASC@gmail.com>
2022-07-11 10:00:18 -0400

    still trying different file path congifgs

--- api/index.js
+++ api/index.js
@@ -55,7 +55,7 @@ app.use("/api/posts", postRoute)
 app.use(express.static(path.join(__dirname, "/client2/build")));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client2/build/public', 'index.html'));
+  res.sendFile(path.join(__dirname, '/client2/build', 'index.html'));
 });
 
 
